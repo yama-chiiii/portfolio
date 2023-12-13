fa15049566960c7f0c348549b829272465d6c4a7
yama-chiiii <nagayama0409@gmail.com>
2023-12-14 00:55:08 +0900

    フォントの導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
     return (
-        <div className="flex justify-center items-center">
-            <div>test</div>
+        <div className="w-full h-screen bg-red-200">
+            <header className="h-16 flex justify-between bg-light-pink">
+                <img src="/heart.svg" alt="Heart" style={{ width: "auto", height: "64px" }} />
+                <div className="flex flex-row">
+                    <div className="mx-4 font-Dot">About</div>
+                    <div className="mx-4 font-Dot">Skill</div>
+                </div>
+            </header>
+            <div className="flex justify-center items-center"></div>
         </div>
     );
 }
