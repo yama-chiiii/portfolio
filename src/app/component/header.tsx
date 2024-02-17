d9999b1e4c5886c548b26ec5881287f70900812d
yama-chiiii <nagayama0409@gmail.com>
2024-02-17 20:28:26 +0900

    ハンバーガーメニューの作成

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -1,11 +1,50 @@
-export const Header = () => {
+import { useEffect, useState } from "react";
+
+type OnHistoryButtonClick = () => void;
+
+type Props = {
+    onHistoryButtonClick: OnHistoryButtonClick;
+};
+
+export const Header = ({ onHistoryButtonClick }: Props) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    useEffect(() => {
+        const handleOutsideClick = (e: MouseEvent) => {
+            const targetElement = e.target as HTMLElement;
+            if (!targetElement.closest(".menu-container")) {
+                setMenuOpen(false);
+            }
+        };
+
+        document.addEventListener("click", handleOutsideClick);
+
+        return () => {
+            document.removeEventListener("click", handleOutsideClick);
+        };
+    }, [menuOpen]);
+
+    const Menu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <div>
             <header className="h-92 flex justify-between items-center bg-light-pink">
                 <img src="/heart.svg" alt="Heart" style={{ width: "auto", height: "72px" }} className="px-8" />
                 <div className="flex flex-row items-center font-semibold text-4xl text-vivid-pink">
-                    <button className="font-Dot">Profile</button>
-                    <button className="px-32 font-Dot">Skill</button>
+                    <button className="font-Dot" onClick={Menu}>
+                        ☰
+                    </button>
+                    {menuOpen && (
+                        <div className="absolute top-0 right-0 mt-16 mr-8 flex flex-col bg-white border border-gray-300 rounded-md shadow-md">
+                            <button className="px-32 font-Dot" onClick={onHistoryButtonClick}>
+                                history
+                            </button>
+                            <button className="px-32 font-Dot">skill</button>
+                            <button className="px-32 font-Dot">language</button>
+                        </div>
+                    )}
                 </div>
             </header>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,21 @@ import { History } from "@/app/component/history";
 import { Profile } from "@/app/component/profile";
 import { Skill } from "@/app/component/skill";
 import { Title } from "@/app/component/title";
+import { useRef } from "react";
 
 export default function Home() {
+    const historyRef = useRef<HTMLDivElement | null>(null);
+
+    // historyボタンが押されたときの処理
+    const handleHistoryButtonClick = () => {
+        if (historyRef.current) {
+            historyRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
     return (
         <div className="w-full min-h-screen bg-light-pink">
-            <Header />
+            <Header onHistoryButtonClick={handleHistoryButtonClick} />
             <div className="flex justify-center mb-12">
                 <div className="w-11/12 h-720 flex justify-center bg-white">
                     <img src="/name.svg" alt="Name" style={{ width: "w-full", height: "auto" }} />
@@ -22,7 +32,10 @@ export default function Home() {
                     <div className="w-full h-520 flex flex-col my-20 bg-white font-Dot font-extrabold text-2xl shadow-pink">
                         <Profile />
                     </div>
-                    <Title title="history" subTitle="これまでのこと" />
+                    <div ref={historyRef}>
+                        <Title title="history" subTitle="これまでのこと" />
+                        <History />
+                    </div>
                     <History />
                     <Title title="skill" subTitle="べんきょうちゅう" />
                     <Skill />
