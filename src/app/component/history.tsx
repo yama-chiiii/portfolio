bff7e4ba65be7667a808f5314a2e52f974b73e3e
yama-chiiii <nagayama0409@gmail.com>
2024-04-01 02:19:59 +0900

    スタイルの調整

--- src/app/component/history.tsx
+++ src/app/component/history.tsx
@@ -1,9 +1,11 @@
+import "./history.css";
+
 export const History = () => {
     return (
         <div className="w-full h-auto z-20 my-20 flex flex-col items-center bg-white font-Dot font-extrabold text-2xl">
             <div className="w-full h-40 bg-light-yellow" />
             <div className="w-full flex flex-row overflow-y-auto">
-                <div className="w-56 flex flex-col mt-62 ml-32">
+                <div className="line">
                     <div className="dot" />
                     <div className="custom-line" />
                     <div className="dot" />
@@ -12,7 +14,7 @@ export const History = () => {
                     <div className="custom-line" />
                     <div className="dot" />
                 </div>
-                <div className="mx-4 my-16">
+                <div className="w-4/5 mr-4 my-16">
                     <h2 className="w-80 my-16 text-vivid-pink border-b-4 border-light-yellow">2021年</h2>
                     <p className="mb-12">４月 　北九州高専 入学 </p>
                     <p className="mb-12">６月 　コンピューター研究部 入部 </p>
--- src/app/component/language.tsx
+++ src/app/component/language.tsx
@@ -7,10 +7,32 @@ type Language = {
 };
 
 export const Language: React.FC<Language> = ({ language, overview, heart }) => {
+    const formatLanguageName = (name: string) => {
+        if (name === "TypeScript") {
+            return (
+                <>
+                    Type
+                    <br />
+                    Script
+                </>
+            ); // TypeScriptの間に改行を挿入
+        } else if (name === "JavaScript") {
+            return (
+                <>
+                    Java
+                    <br />
+                    Script
+                </>
+            ); // JavaScriptの間に改行を挿入
+        } else {
+            return name; // それ以外の言語名はそのまま返す
+        }
+    };
+
     return (
         <div className="w-full flex justify-center">
             <div className="w-4/5 max-h-240 flex flex-col items-center pt-36 rounded-3xl border-t-2 border-r-2 border-l-2 border-b-8 border-purple-pink">
-                <div className="flex flex-wrap text-3xl text-red-pink">{language}</div>
+                <div className="flex flex-wrap text-3xl text-red-pink">{formatLanguageName(language)}</div>
                 <div className="flex flex-wrap text-xs text-black p-12">{overview}</div>
                 <div className="mt-8 mb-16 text-red-600">{heart}</div>
             </div>
